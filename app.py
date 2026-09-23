6798d954cc7f6b6a50b8bebb63ee3e795b07103c
aykutaydogdu11-ctrl <aykutaydogdu11@gmail.com>
2026-09-23 22:30:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Moonrise Order App çalışıyor! 🎉"
+
+if __name__ == "__main__":
+    app.run(debug=True)
